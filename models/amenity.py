f7d365e8f520504b6ae6868453450e72ed4657e6
MartheUwir <marthuwir@gmail.com>
2023-08-15 22:38:00 +0200

    new

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
+"""
+Amenity class, a subclass of BaseModel
+"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an amenity.
-    Attributes:
-        name (str): The name of the amenity.
     """
-
+    A subclass of BaseModel class
+    Public class attribute:
+        name: (str)
+    """
     name = ""
